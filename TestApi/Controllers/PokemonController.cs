4bfd0ab1bb2f47549e8d35d60f7470292823a4a5
Rajesh Kumar Ravisekaran <rajeshkumar.r@prodapt.com>
2021-04-07 15:24:35 +0100

    First Commit

---
+++ TestApi/Controllers/PokemonController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using TestApi.Models;
+
+namespace TestApi.Controllers
+{
+    public class PokemonController : ApiController
+    {
+        // GET api/Pokemon
+        public List<PokemonDetails> Get()
+        {
+            pokemonfulldetails sdList = new pokemonfulldetails();
+            return sdList.pokemonDetails;
+        }
+
+        // GET api/Pokemon/5
+        public string Get(int id)
+        {
+
+            return "value";
+        }
+
+        // POST api/Pokemon
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/Pokemon/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/Pokemon/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TestApi/Models/PokemonDetails.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestApi.Models
+{
+    public class PokemonDetails
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Descritpion { get; set; }
+        public string habitat { get; set; }
+        public string isLegendary { get; set; }
+    }  
+    public class pokemonfulldetails
+    {
+        public List<PokemonDetails> pokemonDetails { get; set; } = null;
+        public pokemonfulldetails()
+        {
+            pokemonDetails = new List<PokemonDetails>();
+            PokemonDetails sd = new PokemonDetails();
+            sd.Id = 1;
+            sd.Name = "mewtwo";
+            sd.Descritpion = "Created by a scientist aftern years of horrific genesplicing and dna engineering experiments,it was.";
+            sd.habitat = "rare";
+            sd.isLegendary = "True";
+            
+            pokemonDetails.Add(sd);
+        }
+    }
+}
